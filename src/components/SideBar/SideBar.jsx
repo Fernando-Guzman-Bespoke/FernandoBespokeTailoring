dea0f77a12743e9980ddbbb4d15200453bd4af01
DanielMorales1103 <91585288+DanielMorales1103@users.noreply.github.com>
2023-05-10 07:31:49 -0600

    SideBar
    Diseño de SideBar finalizado

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -6,10 +6,18 @@ export default function SideBar(){
             <div className='SideBar'>  
                 <div className='BoxUser'>
                     <img className='ImgAdminUser' src={AdminUser}></img>
+                    <div className='BoxUserText'>
+                        <div className='UserText'>User</div>
+                        <div className='AdminText'>Admin</div>
+                    </div>
                 </div>
-                <div>
-                    
+                <div className='Boxpedidos'>
+                    <div className='TitlePedidos'>MANEJO DE PEDIDOS</div>
+                    <div className='ModulosText'>Módulo 2</div>
+                    <div className='ModulosText'>Módulo 3</div>
+                    <div className='ModulosText'>Módulo 4</div>
                 </div>
+                
             </div>
         </>
     )
