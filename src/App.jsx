1ff6151fbcfad51c084dd616ca8a4b5a24b3da61
DanielMorales1103 <91585288+DanielMorales1103@users.noreply.github.com>
2023-05-10 18:31:54 -0600

    Manejo de Inventario
    Corrección de botones

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import './App.css'
 // import MeasurePage from './pages/MeasurePage'
 import InventoryPage from './pages/InventoryPage'
-import MeasurePage from './pages/MeasurePage'
-import MuestrarioTelas from './pages/MuestrarioTelas'
 function App() {
 
   return (
     <>
       {/* <MeasurePage></MeasurePage> */}
       <InventoryPage></InventoryPage>
-
-      {/* <MuestrarioTelas></MuestrarioTelas>*/}
     </>
   )
 }
