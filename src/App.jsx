bd9e6abed5f2614975c8a7f2d4ecdf5f57d81fc0
DanielMorales1103 <91585288+DanielMorales1103@users.noreply.github.com>
2023-05-22 17:15:27 -0600

    Listado de Pedidos
    Creación de la vista de Listado de Pedidos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 // import MeasurePage from './pages/MeasurePage'
-import InventoryPage from './pages/InventoryPage'
+// import InventoryPage from './pages/InventoryPage'
+import OrderHandlingPage from './pages/OrderHandlingPage'
 function App() {
 
   return (
     <>
       {/* <MeasurePage></MeasurePage> */}
-      <InventoryPage></InventoryPage>
+      {/* <InventoryPage></InventoryPage> */}
+      <OrderHandlingPage></OrderHandlingPage> 
     </>
   )
 }
