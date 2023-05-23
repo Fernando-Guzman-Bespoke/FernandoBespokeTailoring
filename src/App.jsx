96156d35f8d9ca6b3227b0d038c6b607285af206
DanielMorales1103 <91585288+DanielMorales1103@users.noreply.github.com>
2023-05-23 16:25:14 -0600

    Cotizaciones
    Creación de vista de cotizaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import './App.css'
 // import MeasurePage from './pages/MeasurePage'
 // import InventoryPage from './pages/InventoryPage'
-import OrderHandlingPage from './pages/OrderHandlingPage'
-
+// import OrderHandlingPage from './pages/OrderHandlingPage'
+import PricePage from './pages/PricePage'
 function App() {
+
   return (
     <>
       {/* <MeasurePage></MeasurePage> */}
       {/* <InventoryPage></InventoryPage> */}
-      <OrderHandlingPage></OrderHandlingPage> 
-      <MuestrarioDisenos></MuestrarioDisenos>
+      {/* <OrderHandlingPage></OrderHandlingPage>  */}
+      <PricePage></PricePage>
     </>
   )
 }
---
+++ src/pages/PricePage.jsx
@@ -0,0 +1,89 @@
+/* eslint-disable react/jsx-key */
+import Header from '../components/header/header'
+import PageTitle from '../components/PageTitle/PageTitle'
+import SideBar from '../components/SideBar/SideBar'
+import lapiz from "../assets/lapiz.svg"
+import plus from "../assets/plus.svg"
+import minus from "../assets/minus.svg"
+import Input from '../components/Input/input'
+import Boton from '../components/botones/botones'
+
+
+import './PricePage.css'
+
+export default function PricePage(){
+    return(
+        <>
+            <Header></Header>
+            <div className='PricePageContainer'>
+                <SideBar TitleSide={'FACTURACIÓN'}></SideBar>
+                <div className='RightContainer'>
+                    <PageTitle title={'Cotización'}></PageTitle>
+                    <div className='whiteContainer'>
+                        <div className='WhiteInternContainer'>
+                            <div className='divHorizontal'>
+
+                                <div className='TextBoxContainer-left'>
+                                    <p className='TextTitle'>Diseño</p>
+                                    <div className='BoxContainer'>
+                                        <div className='TextContainer'>
+                                            <div className='text'>Traje Completo</div>
+                                            <div className='subtext'>Caballero</div>
+                                        </div>
+                                        <img className='imgLapiz' src={lapiz}></img>
+                                    </div>
+                                </div>
+
+                                <div className='TextBoxContainer-right'>
+                                    <p className='TextTitle'>Tela</p>
+                                    <div className='BoxContainer'>
+                                        <div className='Onlytext'>Dracón</div>
+                                        <div className='textCant'> 5 yd<sup>2</sup></div>
+                                        <img className='imgLapiz' src={lapiz}></img>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className='divHorizontal'>
+
+                                <div className='TextBoxContainer-left'>
+                                    <p className='TextTitle'>Cliente</p>
+                                    <div className='BoxContainer'>
+                                        <div className='Onlytext'>Enrique Gonzáles</div>
+                                        <img className='imgLapiz' src={lapiz}></img>
+                                    </div>
+                                </div>
+
+                                <div className='TextBoxContainer-right'>
+                                    <p className='TextTitle'>Cantidad</p>
+                                    <div className='BoxContainerNums'>
+                                        <img className='imgPLus' src={minus}></img>
+                                        <div className='num'>1</div>
+                                        <img className='imgMinus' src={plus}></img>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className='divHorizontal'>
+
+                                <div className='TextBoxContainer-left'>
+                                    <p className='TextTitle'>Extra</p>
+                                    <Input clase={'input2'} value={'Detalles extra (opcional)'}></Input>
+                                </div>
+                                <div className='Subtotal'>Subtotal: Q4,000.00</div>
+
+                            </div>  
+
+                            <div className='ContainerButtons3'>
+                                <Boton clase={'BotonGuardarCotizacion'} text={'Guardar Cotización'}></Boton>
+                                <Boton clase={'BotonCancelarCotizacion'} text={'Cancelar'}></Boton>
+                                <Boton clase={'BotonhacerPedido'} text={'Hacer pedido'}></Boton>
+                            </div> 
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
