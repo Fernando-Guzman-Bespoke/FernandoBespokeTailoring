9aa7f37e5b6b8eb6463726ea5779c1b7cf9a0565
Brandon Sicay <sicayb@gmail.com~>
2023-07-22 22:28:32 -0600

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,48 @@
-import './App.css'
+// import './App.css'
+// import MeasurePage from './pages/MeasurePage'
+// // import InventoryPage from './pages/InventoryPage'
+// // import OrderHandlingPage from './pages/OrderHandlingPage'
+// //import PricePage from './pages/PricePage'
+// //import OrderHandlingPage from './pages/OrderHandlingPage'
+// // import AgregarTela from './pages/AgregarTela'
+// //import MuestrarioTelas from './pages/MuestrarioDisenos'
+// import IngresoSaco from './pages/IngresoSaco'
+// import Login from './pages/Login'
+
+// function App() {
+
+//   return (
+//     <>
+//        {/* <MeasurePage></MeasurePage>  */}
+//       {/* <InventoryPage></InventoryPage> */}
+//       {/* <OrderHandlingPage></OrderHandlingPage>  */}
+//       {/* <AgregarTela></AgregarTela> */}
+//       {/* <MuestrarioTelas></MuestrarioTelas> */}
+//       <Login></Login>
+//        {/*<PricePage></PricePage>}
+//       {/*<AgregarTela></AgregarTela>*/}
+//       {/* <MuestrarioTelas></MuestrarioTelas> */}
+//       {/* <IngresoSaco></IngresoSaco> */}
+//     </>
+//   )
+// }
+
+// export default App
+
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import MeasurePage from './pages/MeasurePage'
-// import InventoryPage from './pages/InventoryPage'
-// import OrderHandlingPage from './pages/OrderHandlingPage'
-//import PricePage from './pages/PricePage'
-//import OrderHandlingPage from './pages/OrderHandlingPage'
-// import AgregarTela from './pages/AgregarTela'
-//import MuestrarioTelas from './pages/MuestrarioDisenos'
-import IngresoSaco from './pages/IngresoSaco'
+// import './App.css'
 import Login from './pages/Login'
 
 function App() {
 
   return (
-    <>
-       {/* <MeasurePage></MeasurePage>  */}
-      {/* <InventoryPage></InventoryPage> */}
-      {/* <OrderHandlingPage></OrderHandlingPage>  */}
-      {/* <AgregarTela></AgregarTela> */}
-      {/* <MuestrarioTelas></MuestrarioTelas> */}
-      <Login></Login>
-       {/*<PricePage></PricePage>}
-      {/*<AgregarTela></AgregarTela>*/}
-      {/* <MuestrarioTelas></MuestrarioTelas> */}
-      {/* <IngresoSaco></IngresoSaco> */}
-    </>
-  )
+    <Router>
+      <Route path="/login" component={Login} />
+      <Route path="/measure" component={MeasurePage} />
+      <Redirect from="/" to="/Login" />
+    </Router>
+  );
 }
 
 export default App
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useState }from 'react'
 import './Login.css'
 import traje from '../assets/traje.png';
+import { useHistory } from "react-router-dom";
 
 export default function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [showErrorModal, setShowErrorModal] = useState(false);
+    const history = useHistory();
+
+    const handleLogin = async () => {
+        const requestOptions = {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ username, password })
+        };
+    
+        try {
+          const response = await fetch('http://localhost:8082/api/auth/signin', requestOptions);
+          const data = await response.json();
+          console.log(data)
+          localStorage.setItem("token", data.accessToken)
+          
+          if (data.accessToken) {
+            history.push('/measure');
+          } else {
+            setShowErrorModal(true);
+          }
+    
+        } catch (error) {
+          console.error('Hubo un error:', error);
+        }
+      }
+
+      const closeModal = () => {
+        setShowErrorModal(false);
+      }
+
   return (
     <div className='contenido'>
 
@@ -13,13 +47,13 @@ export default function Login() {
 
     <div className='login-container' >
 
-        <h1>Login</h1>
+        <h1 className='LoginTitle'>Login</h1>
         <h2>Bienvenido, por favor ingrese sus credenciales</h2>
         <div className='inputContainer'>
-            <input className="input" type="text" placeholder="Correo" defaultValue=""/>
+            <input className="input" type="text" placeholder="Correo" defaultValue="" onChange={(e) => setUsername(e.target.value)}/>
         </div>
         <div className='inputContainer'>
-            <input className="input" type="text" placeholder="Contraseña" defaultValue=""/>
+            <input className="input" type="text" placeholder="Contraseña" defaultValue="" onChange={(e) => setPassword(e.target.value)}/>
         </div>
         <div className='inputContainer'>
             <input className='check' type='checkbox'/> <p>Recordarme</p>
@@ -27,13 +61,21 @@ export default function Login() {
         </div>
         
         <div className='botones'>
-            <button className='buton' >Iniciar Sesión</button>
+            <button className='buton'  onClick={handleLogin} >Iniciar Sesión</button>
             <button className='buton1' >Registrarse</button>
         </div>
 
     </div>
 
-    
+    { showErrorModal && 
+      <div className='modal'>
+        <div className='modal-content'>
+          <h2>Credenciales incorrectas</h2>
+          <p>El nombre de usuario o la contraseña que ingresaste son incorrectos.</p>
+          <button onClick={closeModal}>Cerrar</button>
+        </div>
+      </div>
+    }
     </div>
   )
 }
